0160e36493edd1c3eb5fe788a0efde40930a4fe4
Gustavo9481 <guscode.apps@gmail.com>
2025-08-21 22:34:47 -0500

    tests | funciones y documentación tests -> model_task

--- tests/models_tests/model_task_test.py
+++ tests/models_tests/model_task_test.py
@@ -60,11 +60,14 @@ def test_task_creation_with_no_defaults():
 
 # TEST: 03
 def test_task_creation_with_invalid_status():
-    """Comprueba error al crear una tarea con un status inválido.
+    """Comprueba error con un status inválido.
 
     Este test verifica que si la tarea es creada con un valor de status no
     válido, la tarea no se crea. Los valores válidos se definen en el módulo
     models/model_task.py en la lista Literal Status.
+
+    Raises:
+        ValidationError: Cuando el status tiene un valor no permitido.
     """
     status_invalid: str = "invalid"
     content_text: str = "Contenido de prueba: status inválido"
@@ -73,8 +76,17 @@ def test_task_creation_with_invalid_status():
         test_instance = Task(status=status_invalid, content=content_text)
 
 
+# TEST: 04
 def test_task_creation_with_invalid_tag():
-    # TEST: creación de tarea con tag inválido.
+    """Comprueba error con un tag inválido.
+
+    El test verifica que si el valor de tag (etiqueta) es un valor distinto a
+    los valores permitidos, la tarea no será generada. Los valores admitidos se
+    definen en el módulo models/model_task.py en el Literal Tag.
+
+    Raises:
+        ValidationError: Cuando tag tiene un valor no permitido.
+    """
     tag_invalid: str = "invalid"
     content_text: str = "Contenido de prueba: tag inválido"
 
@@ -82,38 +94,80 @@ def test_task_creation_with_invalid_tag():
         test_instance = Task(tag=tag_invalid, content=content_text)
 
 
+# TEST: 05
 def test_task_creation_with_invalid_content_type_integer():
-    # TEST: creación de tarea con contenido de tipo inválido (int).
+    """Comprueba error generado con content tipo entero.
+
+    Este test verifica que si el tipo de valor para la propiedad content
+    resulta ser un entero, la tarea no será generada.
+
+    Raises:
+        ValidationError: Cuando content es de tipo int.
+    """
     content_invalid: int = 1234567890
 
     with pytest.raises(ValidationError) as excinfo:
         test_instance = Task(content=content_invalid)
 
 
+# TEST: 06
 def test_task_creation_with_invalid_content_type_float():
-    # TEST: creación de tarea con contenido de tipo inválido (float).
+    """Comprueba error generado con content tipo float.
+
+    El presente test verifica que si una tarea se crea usando un valor de tipo
+    float para la propiedad content, no será creada.
+
+    Raises:
+        ValidationError: Cuando content es de tipo float.
+    """
     content_invalid: float = 1.234567890
 
     with pytest.raises(ValidationError) as excinfo:
         test_instance = Task(content=content_invalid)
 
 
+# TEST: 07
 def test_task_creation_with_invalid_content_type_boolean():
-    # TEST: creación de tarea con contenido de tipo inválido (bool).
+    """Comprueba error generado con content tipo booleano.
+
+    El test verifica que si una tarea es generada con un valor de tipo boleano
+    para su propiedad content, no será generada.
+
+    Raises:
+        ValidationError: Cuando content es de tipo booleano.
+    """
     content_invalid: bool = True
 
     with pytest.raises(ValidationError) as excinfo:
         test_instance = Task(content=content_invalid)
 
 
+# TEST: 08
 def test_task_creation_with_missing_content():
-    # TEST: creación de tarea sin definir el contenido.
+    """Comprueba error generado al no definir content.
+
+    El test verifica que si una tarea es generada sin hacer la definición
+    correspondiente de la propiedad content, no será generada.
+
+    Raises:
+        ValidationError: Cuando no se define la propiedad content.
+    """
     with pytest.raises(ValidationError):
         test_instance = Task(status="pending", tag="personal", priority="baja")
 
 
+# TEST: 09
 def test_task_creation_with_invalid_priority():
-    # TEST: creación de tarea con prioridad inválida.
+    """Comprueba error generado con valor de prioridad inválida.
+
+    El test verifica que si la tarea contiene un valor no permitido en la
+    propiedad priority, no será generada. Los valores permitidos están 
+    definidos en el módulo models/model_task.py en el objeto Literal Priority.
+
+    Raises:
+        ValidationError: Cuando el valor de prioridad no es uno de los
+        permitidos.
+    """
     priority_invalid: str = "invalid"
     content_text: str = "Contenido de prueba: prioridad inválida"
 
